1ff0bdff1903dfbcba1969287eb9272e6ff5ac80
wugs <wugs@pqlabs.com>
2015-01-14 18:40:45 +0800

    update

--- SchemeInterpreterHs/interpreter.hs
+++ SchemeInterpreterHs/interpreter.hs
@@ -3,11 +3,42 @@ import Data.String.Utils
 import Data.List.Utils
 
 data LispVal = Atom String
-             | List [LispVal]
-             | DottedList [LispVal] LispVal
              | Number Integer
              | String String
              | Bool Bool
+             | List [LispVal]
+               deriving (Eq, Ord, Show)
+
+lv1 = List [Atom "define", Atom "x", Number 10]
+lv2 = List [Atom "define", Atom "y"]
+lv3 = List [Atom "define", List [Atom "add", Atom "x", Atom "y"], List [Atom "+", Atom "x", Atom "y"]]
+
+data T = ConsInt Int
+       | ConsString String
+       | ConsChar Char
+         deriving (Show)
+
+tt = [ConsInt 42, ConsChar 'a', ConsString "foo"]
+
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
+           deriving (Eq, Ord, Show, Read, Bounded, Enum)
+data Tree = Empty
+          | Leaf Int
+          | Node Tree Tree
+
+depth :: Tree -> Int
+depth Empty = 0
+depth (Leaf n) = 1
+depth (Node l r) = 1 + max (depth l) (depth r)
+
+--data List a = Nil | Cons a (List a) deriving (Show)
+data Expression = Numb Int
+                | Add Expression Expression
+                | Minus Expression Expression
+                | Mult Expression Expression
+                | Divide Expression Expression
+                  deriving (Show)
+
 
 e1 = "(define x 10)"
 e2 = "(define twice (lambda (x) (* 2 x)))"
@@ -18,14 +49,21 @@ e5 = "(define (add x y) (+ x y))"
 tokenize :: String -> [String]
 tokenize exp = filter (\x -> (not $ null x)) (split " " (replace ")" " ) " (replace "(" " ( " exp)))
 
-readtokens :: [String] -> [String]
-readtokens [] = []
---readtokens [x:xs] = if x == "("
+{-
+readtokens :: [String] -> LispVal
+readtokens [] = List []
+readtokens (x:xs) =
+    if x == "(" then readtokens xs
+    else if x == ")" then readtokens xs
+    else List ([Atom x] ++ (readtokens xs))
+-}
+
 
 t1 = tokenize e1
 t2 = tokenize e2
 t3 = tokenize e3
 t4 = tokenize e4
+t5 = tokenize e5
 
 
 
